cec49e391e0aa494df05e9bc210c12408357173d
aryanbelle <aryanbelle692@gmail.com>
2024-12-09 22:38:05 +0530

    Basic implementaions using HTML

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from django.shortcuts import get_object_or_404
 
 def task_list(request):
     tasks = Task.objects.all()
@@ -16,3 +17,10 @@ def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
     return redirect('task_list')
+
+def toggle_task(request, task_id):
+    if request.method == 'POST':
+        task = get_object_or_404(Task, id=task_id)
+        task.completed = not task.completed
+        task.save()
+        return redirect('task_list')
